09e12e9b0287cee4e44afb8beb36a7ccae532a68
ClaudioFritzen <sdfritzen96@gmail.com>
2022-11-05 23:06:58 +0000

    recebendo o resultado da API

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 import requests
+
+#pegamos o link aonde a API esta disponivel
+link = 'https://testapi.claudiofritzen.repl.co/totalvendas'
+print('resultado do link', link)
+# fazendo a requsição para o link
+requisicao = requests.get(link)
+print('Receberemos um codigo pra ver se o site esta no ar', requisicao)
+
+# tranformando o cod recebido em json
+print(requisicao.json())
